35752b2b83136b710ff526eee67818bd82beeb28
alicemartynova94 <alicemartynova94@gmail.ru>
2023-10-31 22:08:56 +0300

    add Rental entity class

--- src/main/java/org/javarushproject/domain/Rental.java
+++ src/main/java/org/javarushproject/domain/Rental.java
@@ -1,2 +1,45 @@
-package org.javarushproject.domain;public class Rental {
+package org.javarushproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "rental")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "rental_id", nullable = false)
+    private Long id;
+
+    @Column(name = "rental_date")
+    private LocalDateTime rentalDate;
+
+    @ManyToOne
+    @JoinColumn(name = "inventory_id")
+    private Inventory inventory;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Column(name = "return_date")
+    private LocalDateTime returnDate;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
 }

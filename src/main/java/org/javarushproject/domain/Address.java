b9cc9fde331cf28d526569f1b937b25f692520fb
alicemartynova94 <alicemartynova94@gmail.ru>
2023-10-31 18:00:37 +0300

    add entity class Address

--- src/main/java/org/javarushproject/domain/Address.java
+++ src/main/java/org/javarushproject/domain/Address.java
@@ -1,2 +1,46 @@
-package org.javarushproject.domain;public class Address {
+package org.javarushproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "address")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id", nullable = false)
+    private Long id;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "address2")
+    private String address2;
+
+    @Column(name = "district", nullable = false)
+    private String district;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
 }

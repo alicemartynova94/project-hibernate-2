6ab2b2f27c26c40cd3cf614cf965e122db5983f1
alicemartynova94 <alicemartynova94@gmail.ru>
2023-10-31 18:05:48 +0300

    add entity class Store with required fields

--- src/main/java/org/javarushproject/domain/Store.java
+++ src/main/java/org/javarushproject/domain/Store.java
@@ -1,2 +1,35 @@
-package org.javarushproject.domain;public class Store {
+package org.javarushproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "store")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id", nullable = false)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "manager_staff_id", nullable = false)
+    private Staff staff;
+
+    @OneToOne
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address address;
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
 }

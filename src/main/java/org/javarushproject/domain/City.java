e7593661daf2db6e989bd7c1aa74a502e5fdd295
alicemartynova94 <alicemartynova94@gmail.ru>
2023-10-31 17:56:07 +0300

    add new entity class City

--- src/main/java/org/javarushproject/domain/City.java
+++ src/main/java/org/javarushproject/domain/City.java
@@ -1,2 +1,34 @@
-package org.javarushproject.domain;public class City {
+package org.javarushproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "city")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id", nullable = false)
+    private Long id;
+
+    @Column(name = "city", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
 }

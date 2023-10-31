62698f9925f078f6b4edb7e09ef2079ede504af8
alicemartynova94 <alicemartynova94@gmail.ru>
2023-10-31 17:25:17 +0300

    add entity class Inventory with required fields

---
+++ src/main/java/org/javarushproject/domain/Inventory.java
@@ -0,0 +1,35 @@
+package org.javarushproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "inventory")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inventory_id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+}
---
+++ src/main/java/org/javarushproject/domain/Store.java
@@ -0,0 +1,2 @@
+package org.javarushproject.domain;public class Store {
+}

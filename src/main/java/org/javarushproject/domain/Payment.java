adb5959f4c56d9bc28ae8d0e7ca71808230adc9a
alicemartynova94 <alicemartynova94@gmail.ru>
2023-10-31 22:19:57 +0300

    add entity class Payment with required fields

--- src/main/java/org/javarushproject/domain/Payment.java
+++ src/main/java/org/javarushproject/domain/Payment.java
@@ -1,2 +1,48 @@
-package org.javarushproject.domain;public class Payment {
+package org.javarushproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "payment")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private Staff staff;
+
+    @OneToOne
+    @JoinColumn(name = "rental_id")
+    private Rental rental;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "payment_date")
+    @CreationTimestamp
+    private LocalDateTime paymentDate;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
 }

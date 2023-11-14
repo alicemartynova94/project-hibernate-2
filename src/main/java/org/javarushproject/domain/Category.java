7d7fdee3e0f652ff42fe21865b015a596fbafdc5
alicemartynova94 <alicemartynova94@gmail.ru>
2023-11-14 21:16:29 +0300

    correct id Type from long to byte due to incorect column type

--- src/main/java/org/javarushproject/domain/Category.java
+++ src/main/java/org/javarushproject/domain/Category.java
@@ -20,7 +20,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id", nullable = false)
-    private Long id;
+    private Byte id;
 
     @Column(name = "name")
     private String name;

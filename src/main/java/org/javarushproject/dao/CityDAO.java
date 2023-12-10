3ea20065d1e3af01fe36f6e5358a5408b6bf5c6f
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-10 18:26:25 +0300

    add new method that helps finding city by name

--- src/main/java/org/javarushproject/dao/CityDAO.java
+++ src/main/java/org/javarushproject/dao/CityDAO.java
@@ -1,11 +1,19 @@
 package org.javarushproject.dao;
 
+import org.hibernate.query.Query;
 import org.hibernate.SessionFactory;
 import org.javarushproject.domain.City;
 
 public class CityDAO extends GenericDAO<City> {
 
-    public CityDAO( SessionFactory sessionFactory) {
+    public CityDAO(SessionFactory sessionFactory) {
         super(City.class, sessionFactory);
+    }
+
+    public City getByName(String name) {
+        Query<City> query = getCurrentSession().createQuery("select c from City c where c.name = :NAME", City.class);
+        query.setParameter("NAME", name);
+        query.setMaxResults(1);
+        return query.getSingleResult();
     }
 }

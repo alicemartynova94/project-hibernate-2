294e8ea381829798cc9574a106769e3d9b72b947
alicemartynova94 <alicemartynova94@gmail.ru>
2023-12-10 21:27:35 +0300

    add setters and getters instead of lombok annotations

--- src/main/java/org/javarushproject/domain/Film.java
+++ src/main/java/org/javarushproject/domain/Film.java
@@ -11,14 +11,16 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.Year;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.isNull;
 
 @Entity
 @Table(schema = "movie", name = "film")
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 public class Film {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -70,10 +72,135 @@ public class Film {
     @ManyToMany
     @JoinTable(name = "film_actor", joinColumns = @JoinColumn(name = "film_id", referencedColumnName = "film_id"),
             inverseJoinColumns = @JoinColumn(name = "actor_id", referencedColumnName = "actor_id"))
-    private Set <Actor> actorSet;
+    private Set<Actor> actorSet;
 
     @ManyToMany
     @JoinTable(name = "film_actor", joinColumns = @JoinColumn(name = "film_id", referencedColumnName = "film_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "category_id"))
-    private Set <Category> categorySet;
+    private Set<Category> categorySet;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Year getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Year releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public Language getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(Language originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public Byte getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(Byte rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public BigDecimal getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(BigDecimal rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public Short getLength() {
+        return length;
+    }
+
+    public void setLength(Short length) {
+        this.length = length;
+    }
+
+    public BigDecimal getReplacementCost() {
+        return replacementCost;
+    }
+
+    public void setReplacementCost(BigDecimal replacementCost) {
+        this.replacementCost = replacementCost;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public Set<Feature> getSpecialFeatures() {
+        if (isNull(specialFeatures) || specialFeatures.isEmpty()) {
+            return null;
+        }
+        Set<Feature> result = new HashSet<>();
+        String[] featuresString = specialFeatures.split(",");
+        for (String feature : featuresString) {
+            result.add(Feature.getFeatureByValue(feature));
+        }
+        result.remove(null);
+        return result;
+    }
+
+    public void setSpecialFeatures(Set<Feature> specialFeatures) {
+        if (isNull(specialFeatures)) {
+            specialFeatures = null;
+        } else {
+            specialFeatures.stream().map(Feature::getValue).collect(Collectors.joining(","));
+        }
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Set<Actor> getActorSet() {
+        return actorSet;
+    }
+
+    public void setActorSet(Set<Actor> actorSet) {
+        this.actorSet = actorSet;
+    }
+
+    public Set<Category> getCategorySet() {
+        return categorySet;
+    }
+
+    public void setCategorySet(Set<Category> categorySet) {
+        this.categorySet = categorySet;
+    }
 }
